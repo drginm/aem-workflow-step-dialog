a301ae16c2a884652dc57dfa515739aca6bd80cc
drginm <drginm>
2019-09-25 18:45:21 -0500

    First version for the custom worflow step process with dialog

---
+++ ui.frontend/clientlib.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/workflow-step-dialog/clientlibs",
+
+    libs: [
+        {
+            name: "clientlib-dependencies",
+            allowProxy: true,
+            categories: ["workflow-step-dialog.dependencies"],
+            serializationFormat: "xml",
+            cssProcessor : ["default:none", "min:none"],
+            jsProcessor: ["default:none", "min:none"],
+            assets: {
+                js: [
+                    "dist/clientlib-dependencies/*.js",
+                ],
+                css: [
+                    "dist/clientlib-dependencies/*.css"
+                ]
+            }
+        },
+        {
+            name: "clientlib-site",
+            allowProxy: true,
+            categories: ["workflow-step-dialog.site"],
+            dependencies: ["workflow-step-dialog.dependencies"],
+            serializationFormat: "xml",
+            cssProcessor : ["default:none", "min:none"],
+            jsProcessor: ["default:none", "min:none"],
+            assets: {
+                js: [
+                    "dist/clientlib-site/*.js",
+                ],
+                css: [
+                    "dist/clientlib-site/*.css"
+                ]
+            }
+        }
+    ]
+};
